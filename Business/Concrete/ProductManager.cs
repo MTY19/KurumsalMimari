8ff888991bd9c4648cfec181009e11a46c98c4e3
Tahir Yıldırım <tahiryildirim19@gmail.com>
2021-11-06 00:26:21 +0300

    Success and Error

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
@@ -23,34 +24,35 @@ namespace Business.Concrete
         {
             //Business codes(daha önce eklenen bir şeyin bir daha eklememesi vb şeyleri yapabiliriz)
             _productDal.Add(product);
-            return new SuccessResult("Ürün Başarıyla Eklendi");
+            return new SuccessResult(Messages.ProductAdded);
         }
 
         public IResult Delete(Product product)
         {
+            //magic string
             _productDal.Delete(product);
-            return new SuccessResult("Ürün Başarıyla Silindi");
+            return new SuccessResult(Messages.ProductDeleted);
         }
 
         public IResult Update(Product product)
         {
             _productDal.Update(product);
-            return new SuccessResult("Ürün Başarıyla Güncellendi");
+            return new SuccessResult(Messages.ProductUpdated);
         }
 
         public IDataResult<Product> GetById(int productId)
         {
-            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId),Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetList()
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
+            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList(), Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetListByCategory(int categoryId)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetList(p => p.CategoryId == categoryId).ToList());
+            return new SuccessDataResult<List<Product>>(_productDal.GetList(p => p.CategoryId == categoryId).ToList(), Messages.ProductAdded);
         }
 
   
